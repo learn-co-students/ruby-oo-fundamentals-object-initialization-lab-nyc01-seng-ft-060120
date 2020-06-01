62864f4694776ee65cfaf612494a6d2ac99ba540
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-06-01 17:50:50 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+    def initialize(name)
+        @name = name
+    end
+end
+
+
+# Define a `Person` class in `lib/person.rb` that provides an `#initialize` method
+# that accepts an argument for the person's name. That argument should be stored
+# within a `@name` instance variable.
